b5992c5b094a1a7f10811d7937919d0cd53e71db
Jimmy Walker <79887392+Jwalker5577@users.noreply.github.com>
2024-03-03 23:58:35 -0500

    Add files via upload

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
-console.log('testing');
+function myFunction() {
+    alert('Button was clicked!');
+  }
+function myClick() {
+    console.log('sup');
+}
+
+const hamburger = document.querySelector('.hamburger');
+hamburger.addEventListener('click', function (){
+  this.classList.toggle('is-active')
+})
+//alert("this is alert");
